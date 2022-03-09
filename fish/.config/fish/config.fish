1dddee50bca53b507fd6a06a8fe4879366dec81c
Bobby Zhou <bobbyzhou22@gmail.com>
2022-03-09 14:51:05 -0600

    automated message

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,12 +12,9 @@ if status --is-interactive
     abbr --add --global gg "git add . && git commit -m \"automated message\" && git push origin master"
     abbr --add --global gs git status
     abbr --add --global gl git log
-    abbr --add --global gpod git push origin dev
     abbr --add --global gpom git push origin master
-    abbr --add --global gfom git fetch origin master
-    abbr --add --global gphm git push heroku master
+    abbr --add --global gppom git pull origin master
     abbr --add --global ls ls -la
-    abbr --add --global treea tree -a
     abbr --add --global q exit
     abbr --add --global c clear
     abbr --add --global ns npm start
